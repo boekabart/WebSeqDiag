61feb827ad991633d127ffe0152b8a1e81ebf487
Bart de Boer <bartdb@gmail.com>
2015-07-08 17:03:55 +0200

    Initial Code

---
+++ AutoWSD/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoWSD
+{
+  class Program
+  {
+    private static void Main(string[] args)
+    {
+      WebSeqDiag.ApiHelpers.ImageConverter = WSD.ImageHacks.PngHacks.ClipBottomRemoveWhite;
+      var folder = Path.GetFullPath(args.FirstOrDefault(a => !a.StartsWith("-")) ?? Directory.GetCurrentDirectory());
+      var option = args.Any(s => s.Equals("-r")) ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+      var tasks =
+        Directory.EnumerateFiles(folder, "*.wsd", option)
+          .Select(path => path.Replace(Directory.GetCurrentDirectory() + "\\", ""))
+          .Select(path => WebSeqDiag.ApiHelpers.CreateOrUpdateImageForEachStyle(path, "png"))
+          .ToArray();
+      Task.WhenAll(tasks).Wait();
+    }
+  }
+}
---
+++ WSD.ImageHacks/PngHacks.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace WSD.ImageHacks
+{
+  public static class PngHacks
+  {
+    public static Image AsImage(this byte[] src)
+    {
+      using (var ms = new MemoryStream(src))
+        return Image.FromStream(ms);
+    }
+
+    public static byte[] AsBytes(this Image src)
+    {
+      using (var ms = new MemoryStream())
+      {
+        src.Save(ms, ImageFormat.Png);
+        src.Dispose();
+        return ms.ToArray();
+      }
+    }
+
+    public static byte[] ClipBottom(this byte[] src)
+    {
+      return src.AsImage().ClipBottom().AsBytes();
+    }
+
+    public static byte[] ClipBottomRemoveWhite(this byte[] src)
+    {
+      return src.AsImage().ClipBottom().RemoveWhite().AsBytes();
+    }
+
+    public static Bitmap ClipBottom(this Image src)
+    {
+      var cropRect = new Rectangle(0, 0, src.Width, src.Height - 16);
+      var target = new Bitmap(cropRect.Width, cropRect.Height);
+
+      using (var g = Graphics.FromImage(target))
+      {
+        g.DrawImage(src, new Rectangle(0, 0, target.Width, target.Height),
+          cropRect,
+          GraphicsUnit.Pixel);
+      }
+      return target;
+    }
+
+    public static Bitmap RemoveWhite(this Image src)
+    {
+      var target = new Bitmap(src);
+      target.MakeTransparent(Color.White);
+      return target;
+    }
+  }
+}
